1a8a8fec0f9501b20ea5658caeca0cf46b651531
Dziedzic Paweł <pawel.dz.m@interia.pl>
2021-05-26 16:25:18 +0200

    2.1. Add test shouldGetDiscount()

--- src/test/java/dziedzic/pawel/tdd/bill/BillTests.java
+++ src/test/java/dziedzic/pawel/tdd/bill/BillTests.java
@@ -14,9 +14,21 @@ public class BillTests {
     public void createBill() {
         this.bill = new Bill(0, "");
     }
+
     @Test
     public void shouldCreateBill() {
         Assertions.assertEquals(bill.getStartAmount(), 0);
         Assertions.assertEquals(bill.getDiscountCode(), "");
+    }
+
+    @Test
+    public void shouldGetDiscount() {
+        String discountCode = bill.provideDiscountCode("Sale-2021");
+        double totalAmount = bill.provideTotalPrice(2000);
+        boolean acceptDiscount = bill.checkDiscount(discountCode, totalAmount);
+        double calculateDiscount = bill.calculateDiscount(discountCode, totalAmount, acceptDiscount);
+
+        Assertions.assertTrue(acceptDiscount);
+        Assertions.assertEquals(1800, calculateDiscount);
     }
 }
