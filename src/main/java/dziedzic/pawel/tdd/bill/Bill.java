f28f9bac0fbc8e8a34a7cd1b067057714c618288
Dziedzic Paweł <pawel.dz.m@interia.pl>
2021-05-26 16:38:29 +0200

    2.2. Test shouldGetDiscount() passed

--- src/main/java/dziedzic/pawel/tdd/bill/Bill.java
+++ src/main/java/dziedzic/pawel/tdd/bill/Bill.java
@@ -16,4 +16,27 @@ public class Bill {
     public String getDiscountCode() {
         return discountCode;
     }
+
+    public String provideDiscountCode(String discountCode) {
+        return discountCode;
+    }
+
+    public double provideTotalPrice(double totalPrice) {
+        return totalPrice;
+    }
+
+    public boolean checkDiscount(String discountCode, double totalPrice) {
+        String providedDiscountCode = this.provideDiscountCode(discountCode) ;
+        double providedTotalPrice = this.provideTotalPrice(totalPrice);
+        if(providedDiscountCode.equals("Sale-2021") && providedTotalPrice>=2000)
+            return true;
+        else
+            return false;
+    }
+    public double calculateDiscount(String discountCode, double totalPrice, boolean acceptDiscount) {
+        if(acceptDiscount && discountCode.equals("Sale-2021"))
+            return totalPrice* 0.9;
+        else
+            return totalPrice;
+    }
 }
