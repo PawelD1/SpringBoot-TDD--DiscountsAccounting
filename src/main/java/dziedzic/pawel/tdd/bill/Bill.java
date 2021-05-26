350607c9d37e09094cd7154aae816a7433252635
Dziedzic Paweł <pawel.dz.m@interia.pl>
2021-05-26 16:07:19 +0200

    1.1 Test shouldCreateBill() passed

---
+++ src/main/java/dziedzic/pawel/tdd/bill/Bill.java
@@ -0,0 +1,14 @@
+package dziedzic.pawel.tdd.bill;
+
+public class Bill {
+    private final double startAmount=0;
+    private final String discountCode="";
+
+    public double getStartAmount() {
+        return startAmount;
+    }
+
+    public String getDicountCode() {
+        return discountCode;
+    }
+}
--- src/test/java/dziedzic/pawel/tdd/bill/BillTests.java
+++ src/test/java/dziedzic/pawel/tdd/bill/BillTests.java
@@ -9,6 +9,7 @@ public class BillTests {
 
     @Test
     public void shouldCreateBill() {
+        Bill bill = new Bill();
         Assertions.assertEquals(bill.getStartAmount(), 0);
         Assertions.assertEquals(bill.getDicountCode(), "");
     }
